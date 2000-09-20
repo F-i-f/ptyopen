34ca16f4db9f50fa50d3999e357483288bcce7d2
Philippe Troin <phil+github-commits@fifi.org>
2000-09-20 00:43:08 +0000

    Added config.h and SVr4 pty functions detection.

--- ptyopen.c
+++ ptyopen.c
@@ -1,3 +1,25 @@
+/*
+  ptyopen - runs a program on a fake terminal
+  ptyopen.c - main program
+  Copyright (C) 1999-2000 Phlippe Troin <phil@fifi.org>
+
+  $Id$
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 #define _GNU_SOURCE
 
 #include <stdlib.h>
@@ -14,6 +36,10 @@
 #include <grp.h>
 #include <fcntl.h>
 #include <errno.h>
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
 
 static char CVSID[] __attribute__((unused))="$Header$";
 
